1ef1bec61c9c5f04614b47ee6e38f1c468caa460
Allen Luo <movoin@gmail.com>
2018-12-24 17:57:14 +0800

    fixed bug

--- conf/bin/service.d/supervisor.d/10-init.sh
+++ conf/bin/service.d/supervisor.d/10-init.sh
@@ -4,4 +4,4 @@
 source "$DOCKER_CONF_PATH/bin/function.sh"
 
 # Replace markers
-replaceFile "<DOCKER_CONF_PATH>" "$WEB_DOCKER_CONF_PATH" "$DOCKER_CONF_PATH/etc/supervisor.d"
+replaceFile "<DOCKER_CONF_PATH>" "$DOCKER_CONF_PATH" "$DOCKER_CONF_PATH/etc/supervisor.d"
